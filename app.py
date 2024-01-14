456fb69922ba8b7bb9ca076aa492d6b33aae4c71
RitikBhambid <you@example.com>
2024-01-14 19:49:39 +0530

    app file created read to host

--- app.py
+++ app.py
@@ -44,3 +44,29 @@ def input_image_setup(uploaded_file):
 
 
 ##initialize our streamlit app
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Gemini Application")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about the image")
+
+input_prompt = """
+               You are an expert in understanding invoices.
+               You will receive input images as invoices &
+               you will have to answer questions based on the input image
+               """
+
+## If ask button is clicked
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
